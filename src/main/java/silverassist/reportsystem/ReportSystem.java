55462f29d5a8bdd771222f10108292e2b313948d
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-02-24 18:24:17 +0900

    MainSystem,Discord保管、取得の変数、関数追加

--- src/main/java/silverassist/reportsystem/ReportSystem.java
+++ src/main/java/silverassist/reportsystem/ReportSystem.java
@@ -1,33 +1,29 @@
 package silverassist.reportsystem;
 
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import silverassist.reportsystem.command.Report;
+import silverassist.reportsystem.reportSystem.MainSystem;
 
-import javax.security.auth.login.LoginException;
-import java.util.HashMap;
-import java.util.List;
+import java.util.logging.Logger;
 
 public final class ReportSystem extends JavaPlugin {
     private static JDA jda = null;
     private static Discord discord = null;
     private static ReportSystem plugin;
+    private static MainSystem MAIN_SYSTEM;
+    private static Logger log;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.saveDefaultConfig();
 
+        log = getLogger();
         discord = new Discord(this);
         jda = discord.startBot();
         plugin = this;
-
-        new Report("report", discord);
+        MAIN_SYSTEM = new MainSystem(this,discord);
+        new Command("report", MAIN_SYSTEM);
     }
 
     @Override
@@ -37,7 +33,10 @@ public final class ReportSystem extends JavaPlugin {
     }
 
     public static JavaPlugin getInstance(){return plugin;}
+    public static Discord getDiscord(){return discord;}
     public static JDA getJDA(){return jda;}
+    public static MainSystem getMainSystem(){return MAIN_SYSTEM;}
+    public static Logger getLog(){return log;}
 
 
 
